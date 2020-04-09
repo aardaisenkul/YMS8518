ef13874d2d33c6b703536bbacdba41ef54ccb9b4
aardaisenkul <aarda.isenkul@tedu.edu.tr>
2020-04-09 16:31:15 +0300

    WCF-master eklendi

---
+++ WCF-master/WCFServer/Data/IChat.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Singletons
+    {
+
+        public static List<MessageDto> Messages = new List<MessageDto>();
+    }
+    [ServiceContract]
+    public interface IChat
+    {
+        [OperationContract]
+        void SendMessage(string nickname, string message);
+
+        [OperationContract]
+        List<MessageDto> GetAllMessages();
+    }
+
+
+
+    public class ChatService : IChat
+    {
+
+
+        public void SendMessage(string nickname, string message)
+        {
+            
+            Singletons.Messages.Add(new MessageDto() { 
+            
+            Id=Guid.NewGuid(),
+            Nickname=nickname,
+            Message = message,
+            SendDate=DateTime.Now
+            
+            
+            });
+            Console.WriteLine(nickname + ": " + message );
+
+            string path = @"C:\Users\ardai\Desktop\YMS8518\WCF-master\log.txt";
+
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                string createText = nickname + ": " + message +"--> "+ DateTime.Now + Environment.NewLine;
+                File.WriteAllText(path, createText);
+            }
+
+            // This text is always added, making the file longer over time
+            // if it is not deleted.
+            string appendText = nickname + ": " + message + "--> " + DateTime.Now + Environment.NewLine;
+            File.AppendAllText(path, appendText);
+
+            //File.writealltext
+            //file.appendtext
+        }
+
+        public List<MessageDto> GetAllMessages()
+        {
+
+            return Singletons.Messages;
+        }
+           
+    }
+
+    public class MessageDto
+    {
+        public Guid Id { get; set; }
+        public DateTime SendDate { get; set; }
+
+        public string Nickname { get; set; }
+        public string Message { get; set; }
+    }
+}
